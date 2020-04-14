e9083ee26c48db863c0bc177d3b0547463ef1747
ArpitSingla <arpitsingla1999@gmail.com>
2020-04-14 11:04:58 +0530

    Updated Construct the String.cpp

--- Codeforces Contest/Round #634 (Div. 3)/Construct the String.cpp
+++ Codeforces Contest/Round #634 (Div. 3)/Construct the String.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin) ;
-    freopen("output.txt", "w", stdout) ;
-    #endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL) ; cout.tie(NULL) ;
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin) ;
+    // freopen("output.txt", "w", stdout) ;
+    // #endif
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL) ; cout.tie(NULL) ;
 
     int t ;
     cin >> t ;
@@ -17,33 +17,17 @@ int main() {
         cin>>n>>a>>b;
         string s;
         char alpha='a';
-        if(a==b){
-            for(int i=0;i<n;i++){
-                s.push_back(alpha+i);
-            }
-            cout<<s<<endl;
-            continue;
-        }
         string temp;
         temp.push_back(alpha);
         for(int i=1;i<b;i++){
             temp.push_back(alpha+i);
         }
         int ans=n/b;
-        for(int i=0;i<ans;i++){
-            s=s+temp;
+        for(int i=0;i<n;i++){
+            s=s+temp[i%b];
         }
-        if(n%b==0){
-            cout<<s<<endl;
-        }
-        else{
-            for(int i=0;i<n%b;i++){
-                s=s+temp[i];
-            }
-            cout<<s<<endl;
-        }
+		cout<<s<<endl;
     }
-
     return 0 ;
-
 }
+
