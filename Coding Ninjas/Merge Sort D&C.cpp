7dd7f3bebbaaf5085bd4562a1ca874104e519845
ArpitSingla <arpitsingla1999@gmail.com>
2020-05-13 11:40:42 +0530

    Finally Understood Merge Sort

---
+++ Coding Ninjas/Merge Sort D&C.cpp
@@ -0,0 +1,32 @@
+void merge(int arr[], int l, int m, int r)
+{
+    int i=l;
+    int j=m+1;
+    int k=0;
+    int temp[r+1];
+    while((i<=m) && (j<=r)){
+        if(arr[i]<=arr[j]){
+            temp[k++]=arr[i++];
+        }
+       else{
+            temp[k++]=arr[j++];
+        }
+    }
+    while(i<=m){
+        temp[k++]=arr[i++];
+    }
+    while(j<=r){
+        temp[k++]=arr[j++];
+    }
+    for(int i=l,j=0;i<=r;i++,j++){
+        arr[i]=temp[j];
+    }
+}
+void mergeSort(int arr[], int l, int r) {
+    if (l < r)   {
+        int m = l+(r-l)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+} 
