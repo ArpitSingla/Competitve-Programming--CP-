827eed73e8bf8bf19cbfc5dfa12dfd81b2421d65
ArpitSingla <arpitsingla1999@gmail.com>
2020-06-28 23:58:37 +0530

    Easy Question

---
+++ Coding Ninjas/Binary Trees/Find a node.cpp
@@ -0,0 +1,33 @@
+// Following is the Binary Tree node structure
+/**************
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+***************/
+
+bool isNodePresent(BinaryTreeNode<int>* root, int x) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    if(root==NULL){
+        return false;
+    }
+	if(root->data==x){
+        return true;
+    }
+    if(isNodePresent(root->left,x) || isNodePresent(root->right,x)){
+        return true;
+    }
+    return false;
+}
