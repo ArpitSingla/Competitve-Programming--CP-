2b46252d9bc61b0ea5965b210a1a003ba59efa7c
ArpitSingla <arpitsingla1999@gmail.com>
2020-09-04 19:45:28 +0530

    Important Question

---
+++ Coding Ninjas/Heaps and Priority Queue/K smallest Elements.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> kSmallest(int *input, int n, int k) {
+	vector<int> ans;
+    priority_queue<int> pq;
+    for(int i=0;i<k;i++){
+        pq.push(input[i]);
+    }
+    for(int i=k;i<n;i++){
+        if(input[i]<pq.top()){
+            pq.pop();
+            pq.push(input[i]);
+        }
+    }
+    while(!pq.empty()){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;
+}
