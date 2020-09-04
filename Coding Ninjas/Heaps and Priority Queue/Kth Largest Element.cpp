17b01c06ae2b73bc6ebf6ef68ae97678c39cbbd7
ArpitSingla <arpitsingla1999@gmail.com>
2020-09-04 19:45:35 +0530

    Important Question

---
+++ Coding Ninjas/Heaps and Priority Queue/Kth Largest Element.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthLargest (vector<int> arr, int n, int k){
+    priority_queue<int,vector<int>,greater<int>> pq(arr.begin(),arr.begin()+k);
+    for(int i=k;i<n;i++){
+        if(arr[i]>pq.top()){
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+    return pq.top();
+}
